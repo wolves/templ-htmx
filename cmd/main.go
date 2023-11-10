7697eb53c9cddc99d579444c788a4f77638239e9
Wolves <cstingl@protonmail.com>
2023-11-09 21:10:45 -0500

    Setup index route with templ

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"net/http"
 
+	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/wolves/templ-htmx/pkg/view"
 )
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-
+		templ.Handler(view.Index("Christopher")).ServeHTTP(w, r)
 	})
 	http.ListenAndServe(":3333", r)
 	// e := echo.New()
