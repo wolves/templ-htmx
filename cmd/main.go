995742e5cc0735b77227913f9853e189375d3cff
Wolves <cstingl@protonmail.com>
2023-11-10 09:05:09 -0500

    Add fileserver for static assets

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
@@ -12,9 +15,16 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		templ.Handler(view.Index("Christopher")).ServeHTTP(w, r)
 	})
+
+	workDir, _ := os.Getwd()
+	filesDir := http.Dir(filepath.Join(workDir, "dist"))
+
+	FileServer(r, "/assets", filesDir)
+
 	http.ListenAndServe(":3333", r)
 	// e := echo.New()
 	//
@@ -35,4 +45,23 @@ func main() {
 	//
 	// e.Logger.Fatal(e.Start(":3333"))
 	//
+}
+
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
